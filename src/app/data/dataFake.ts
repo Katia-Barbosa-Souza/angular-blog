fa9826ee7547f27d6e08ba55588a03f809cc4add
Katia Barbosa de Souza <104703206+Katia-Barbosa-Souza@users.noreply.github.com>
2023-10-19 12:50:02 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":1,
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjluJLutirGM5MNdOLA_Lj1D-e48ow5Re0KQ&usqp=CAU",
+    "title": "Interstellar: Uma equipe de exploradores viaja para al",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum beatae veniam sunt soluta non inventore, neque ea vero repudiandae dolor? Quibusdam ducimus, velit reprehenderit veniam unde pariatur illum distinctio blanditiis?"
+  },
+  {
+    "id":2,
+    "photoCover": "https://proxy.olhardigital.com.br/wp-content/uploads/2023/09/imagem_2023-09-28_165351201-1024x536.png",
+    "title": "Interstellar",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam placeat dolorem, unde pariatur dignissimos cumque aliquid cupiditate id error sed nisi debitis ipsa, tempore recusandae ab porro perferendis vel Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure repellat voluptatum dolorem voluptatem omnis officiis minima? Deleniti in voluptatem eum aliquam esse id! Dicta, assumenda fuga ut odio repellat ipsa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptates quod ex a obcaecati modi maxime explicabo laborum saepe, expedita, consequuntur aliquid pariatur praesentium voluptate ratione at itaque doloremque fugit?"
+  },
+  {
+    "id":3,
+    "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2022/08/senhor_dos_aneis_serie_galadriel__xw9s1i-1210x544.jpg",
+    "title": "O SENHOR DOS ANÉIS: OS ANÉIS DO PODER",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam placeat dolorem, unde pariatur dignissimos cumque aliquid cupiditate id error sed nisi debitis ipsa, tempore recusandae ab porro perferendis vel Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure repellat voluptatum dolorem voluptatem omnis officiis minima? Deleniti in voluptatem eum aliquam esse id! Dicta, assumenda fuga ut odio repellat ipsa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptates quod ex a obcaecati modi maxime explicabo laborum saepe, expedita, consequuntur aliquid pariatur praesentium voluptate ratione at itaque doloremque fugit?"
+  },
+  {
+    "id":4,
+    "photoCover": "https://i0.wp.com/cinestera.com.br/wp-content/uploads/2019/03/supernatural-18-08-10-img00.jpg?fit=726%2C408&ssl=1",
+    "title": "SUPERNATURAL",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam placeat dolorem, unde pariatur dignissimos cumque aliquid cupiditate id error sed nisi debitis ipsa, tempore recusandae ab porro perferendis vel Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure repellat voluptatum dolorem voluptatem omnis officiis minima? Deleniti in voluptatem eum aliquam esse id! Dicta, assumenda fuga ut odio repellat ipsa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptates quod ex a obcaecati modi maxime explicabo laborum saepe, expedita, consequuntur aliquid pariatur praesentium voluptate ratione at itaque doloremque fugit?"
+  }
+]
